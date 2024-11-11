7b799887c7f80ff09635a89cf9ac7902bc92a656
Navneet Sah <118535034+Navneetsah@users.noreply.github.com>
2024-11-11 12:01:37 +0530

    Create Prime subtraction operation.java

---
+++ Prime subtraction operation.java
@@ -0,0 +1,55 @@
+PROBLEM LINK:
+https://leetcode.com/problems/prime-subtraction-operation/?envType=daily-question&envId=2024-11-11
+
+
+SOLUTION:
+class Solution {
+    public boolean primeSubOperation(int[] nums) {
+        int maxElement = getMaxElement(nums);
+
+        boolean[] sieve = new boolean[maxElement + 1];
+        fill(sieve, true);
+        sieve[1] = false;
+        for (int i = 2; i <= Math.sqrt(maxElement + 1); i++) {
+            if (sieve[i]) {
+                for (int j = i * i; j <= maxElement; j += i) {
+                    sieve[j] = false;
+                }
+            }
+        }
+
+        int currValue = 1;
+        int i = 0;
+        while (i < nums.length) {
+            int difference = nums[i] - currValue;
+
+            if (difference < 0) {
+                return false;
+            }
+
+            if (sieve[difference] == true || difference == 0) {
+                i++;
+                currValue++;
+            } else {
+                currValue++;
+            }
+        }
+        return true;
+    }
+
+    private int getMaxElement(int[] nums) {
+        int max = -1;
+        for (int num : nums) {
+            if (num > max) {
+                max = num;
+            }
+        }
+        return max;
+    }
+
+    private void fill(boolean[] arr, boolean value) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = value;
+        }
+    }
+}
