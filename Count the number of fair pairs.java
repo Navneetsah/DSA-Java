cd992351e5eaee1c5f89b22bcdc911ccbb5d4d67
Navneet Sah <118535034+Navneetsah@users.noreply.github.com>
2024-11-13 09:34:08 +0530

    Create Count the number of fair pairs.java

---
+++ Count the number of fair pairs.java
@@ -0,0 +1,22 @@
+PROBLEM LINK:
+https://leetcode.com/problems/count-the-number-of-fair-pairs/submissions/1451215902/?envType=daily-question&envId=2024-11-13
+
+
+SOLUTION:
+class Solution {
+  public long countFairPairs(int[] nums, int lower, int upper) {
+   
+    Arrays.sort(nums);
+    return countLess(nums, upper) - countLess(nums, lower - 1);
+  }
+
+  private long countLess(int[] nums, int sum) {
+    long res = 0;
+    for (int i = 0, j = nums.length - 1; i < j; ++i) {
+      while (i < j && nums[i] + nums[j] > sum)
+        --j;
+      res += j - i;
+    }
+    return res;
+  }
+}
