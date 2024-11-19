ebe73f0181e6a026fad4c97f287259737db4777e
Navneet Sah <118535034+Navneetsah@users.noreply.github.com>
2024-11-19 17:14:30 +0530

    Create Maximum sum of distinct subarrays with length k.java

---
+++ Maximum sum of distinct subarrays with length k.java
@@ -0,0 +1,37 @@
+PROBLEM LINK:
+https://leetcode.com/problems/maximum-sum-of-distinct-subarrays-with-length-k/submissions/1457184590/?envType=daily-question&envId=2024-11-19
+
+
+SOLUTION:
+class Solution {
+    public long maximumSubarraySum(int[] nums, int k) {
+        int n = nums.length;
+        Set<Integer> elements = new HashSet<>();
+        long currentSum = 0;
+        long maxSum = 0;
+        int begin = 0;
+        
+        for (int end = 0; end < n; end++) {
+            if (!elements.contains(nums[end])) {
+                currentSum += nums[end];
+                elements.add(nums[end]);
+                
+                if (end - begin + 1 == k) {
+                    maxSum = Math.max(maxSum, currentSum);
+                    currentSum -= nums[begin];
+                    elements.remove(nums[begin]);
+                    begin++;
+                }
+            } else {
+                while (nums[begin] != nums[end]) {
+                    currentSum -= nums[begin];
+                    elements.remove(nums[begin]);
+                    begin++;
+                }
+                begin++;
+            }
+        }
+        
+        return maxSum;
+    }
+}
