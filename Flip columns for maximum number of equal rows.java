6aae828965ea64b6778d3020df02581989b40032
Navneet Sah <118535034+Navneetsah@users.noreply.github.com>
2024-11-22 21:01:07 +0530

    Create Flip columns for maximum number of equal rows.java

---
+++ Flip columns for maximum number of equal rows.java
@@ -0,0 +1,22 @@
+PROBLEM LINK:
+https://leetcode.com/problems/flip-columns-for-maximum-number-of-equal-rows/submissions/1460102114/?envType=daily-question&envId=2024-11-22
+
+
+SOLUTION:
+class Solution {
+    public int maxEqualRowsAfterFlips(int[][] mat) {
+        Map<String, Integer> patFreq = new HashMap<>();
+        
+        for (int[] row : mat) {
+            StringBuilder pattern = new StringBuilder();
+            if (row[0] == 0) {
+                for (int bit : row) pattern.append(bit);
+            } else {
+                for (int bit : row) pattern.append(bit ^ 1);
+            }
+            patFreq.merge(pattern.toString(), 1, Integer::sum);
+        }
+        
+        return Collections.max(patFreq.values());
+    }
+}
