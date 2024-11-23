d95a034f91db0e02b8e6df5d46a1b951692ceb11
Navneet Sah <118535034+Navneetsah@users.noreply.github.com>
2024-11-23 08:39:01 +0530

    Create Rotating the box.java

---
+++ Rotating the box.java
@@ -0,0 +1,30 @@
+PROBLEM LINK:
+https://leetcode.com/problems/rotating-the-box/submissions/1460442391/?envType=daily-question&envId=2024-11-23
+
+
+SOLUTION:
+class Solution {
+    public char[][] rotateTheBox(char[][] box) {
+        int ROWS = box.length;
+        int COLS = box[0].length;
+        
+        char[][] res = new char[COLS][ROWS];
+        for(char[] row : res) {
+            Arrays.fill(row, '.');
+        }
+        
+        for (int r = 0; r < ROWS; r++) {
+            int i = COLS - 1;
+            for (int c = COLS - 1; c >= 0; c--) {
+                if (box[r][c] == '#') {
+                    res[i][ROWS - r - 1] = '#';
+                    i--;
+                } else if (box[r][c] == '*') {
+                    res[c][ROWS - r - 1] = '*';
+                    i = c - 1;
+                }
+            }
+        }
+        return res;
+    }
+}
